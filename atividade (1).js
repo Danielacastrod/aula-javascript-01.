4e93b495322d6e567084cfbcebf995a4067fe716
Daniela Castro <136139744+Danielacastrod@users.noreply.github.com>
2023-06-26 17:46:32 -0300

    Início das atividades de JavaScript- Start

---
+++ atividade (1).js
@@ -0,0 +1,19 @@
+     /*Criar duas variáveis usando a palavra-chave var.
+      Criar duas variáveis usando a palavra-chave let.
+      Criar duas constantes usando a palavra-chave const.*/
+      
+      var num1 = 99;
+      var num2 = 01;     
+      console.log(num1);
+      console.log(num2);
+      
+      let nome1 = 'text';
+      let nome2 = 'frase aaaaa'
+      console.log(nome1);
+      console.log(nome2);
+      
+      const letra1 = 'x';
+      const letra2 = 'y';
+      console.log(letra1);
+      console.log(letra2);
+
---
+++ atividade (2).js
@@ -0,0 +1,45 @@
+var numero1 = 10;
+var numero2 = 20;
+console.log(numero1);
+console.log(numero2);
+
+
+var texto1 = 'bhfbhbfh';
+var texto2 = "hdbgyguydh";
+console.log(texto1);
+console.log(texto2);
+
+
+var positivo = true;
+var negativo = false;
+console.log("você está cansada?");
+console.log(positivo);
+console.log("Você está animada?");
+console.log(negativo);
+
+
+var suco = ["morango","caju", "acerola"];
+console.log(suco);
+var roupas = ["blusa","calça", "cinto"];
+console.log(roupas);
+
+
+var moto = {cor: "preto", modelo: "ffgddf", ano: 2023};
+console.log(moto);
+console.log(moto.cor);
+var aviao = {cor: "branco", modelo: "eygge", ano: 2022};
+console.log(aviao);
+console.log(aviao.modelo);
+
+
+
+var valorIndefinido;
+console.log("valorIndefinido: " + valorIndefinido);
+var valorIndef;
+console.log("valorIndef: " + valorIndef);
+
+
+var valorVazio = null;
+console.log("valorVazio: " + valorVazio);
+var valorZerado = null;
+console.log("valorZerado: " + valorZerado);
